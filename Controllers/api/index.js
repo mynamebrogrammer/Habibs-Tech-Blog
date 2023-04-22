ebefeca863b3d59f74fd5688b19a8f808d6dc552
Habib Maksoud <habibmaksoud@gmail.com>
2023-04-21 18:44:11 -0700

    trying to run the server to verify functional server

--- Controllers/api/index.js
+++ Controllers/api/index.js
@@ -1,1 +1,12 @@
 const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const postRoutes = require('./postRoutes');
+const profileRoutes = require('./profileRoute');
+
+
+
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+router.use('/profile', profileRoutes);
+
+module.exports = router;
