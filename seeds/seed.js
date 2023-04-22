48991d08d8a248ebe5b6d99f573b9e668561eef0
Habib Maksoud <habibmaksoud@gmail.com>
2023-04-22 15:57:24 -0700

    fixed postroutes, seed.js, and server.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -2,7 +2,7 @@ const sequelize = require('../config/connection');
 const { User, Posts } = require('../models');
 
 const userData = require('./userData.json');
-const postData = require('./postData.json');
+const postData = require('./postsData.json');
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
