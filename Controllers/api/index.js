4a8fe51d679877544bf2c1ec6a21b9de32047374
Habib Maksoud <habibmaksoud@gmail.com>
2023-04-22 15:48:01 -0700

    server runs, seeds runs but may run into some problems

--- Controllers/api/index.js
+++ Controllers/api/index.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes');
-const profileRoutes = require('./profileRoute');
+// const profileRoutes = require('./profileRoute');
 
 
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
-router.use('/profile', profileRoutes);
+// router.use('/profile', profileRoutes);
 
 module.exports = router;
