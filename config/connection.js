2455a709f90f7e4ab1e3445731e53c3beeb4a9ef
Habib Maksoud <habibmaksoud@gmail.com>
2023-04-30 02:12:49 -0700

    heroku crashing, sequelize or db error, fixing with new method in connection.js

--- config/connection.js
+++ config/connection.js
@@ -18,5 +18,18 @@ if (process.env.JAWSDB_URL) {
     }
   );
 }
+console.log('connection to database established');
+
+// log if there is an error
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+  
 
 module.exports = sequelize;
